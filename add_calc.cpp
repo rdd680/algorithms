45ef142cc6d4b724b1bf1642d0ef4003a4180389
Ryan Decker <Ryan@185.142.20.172.s.wireless.uri.edu>
2019-05-23 16:57:31 -0400

    finished with day 1

---
+++ add_calc.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    int first_num = 0;
+    int sec_num = 0;
+    cout << "Enter the first number that you want add together." << endl;
+    cin >> first_num;
+    cout << "Enter the second number that you want add together." << endl;
+    cin >> sec_num;
+    cout << first_num << " plus " << sec_num << " = " << first_num + sec_num << endl;
+    return 0;
+}
---
+++ hello_name.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string name = "";
+    cout << "What's your name???" << endl;
+    cin >> name;
+    cout << "Why hello there " << name << "." << endl;
+    return 0; 
+}
